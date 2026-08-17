9cfda011a425b5918a09e2db3ece7f9fefadbfb6
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-08-17 11:26:17 +0530

    U

--- src/config/cors-origins.js
+++ src/config/cors-origins.js
@@ -3,9 +3,9 @@ export const ALLOWED_ORIGINS = [
   "http://localhost:3000",
   "http://localhost:5678/",
   "https://ibigdata.in",
-  "https://travel.ibigdata.in",
+  "https://edu.ibigdata.in",
   "https://app.estateai.in",
   "https://estateai.in",
 ];
 
-export const clientUrl="https://travel.ibigdata.in";
+export const clientUrl="https://edu.ibigdata.in";
