a3fff2243d303b242765529209ddd72e63180d8c
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-08-21 11:10:26 +0530

    u

--- src/config/cors-origins.js
+++ src/config/cors-origins.js
@@ -15,4 +15,4 @@ export const ALLOWED_ORIGINS = [
   "https://estateai.in",
 ];
 
-export const clientUrl = "https://propertybulk.ibigdata.in";
+export const clientUrl = "https://consult.ibigdata.in";
